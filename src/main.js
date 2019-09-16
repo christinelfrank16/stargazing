4cf27bcc403a45e55720cfa78b305218e68a80e6
Hailey J Gaylor <48165448+Hgaylor0220@users.noreply.github.com>
2019-09-16 16:10:20 -0700

    added wiki for the constellations in file

--- src/main.js
+++ src/main.js
@@ -8,30 +8,25 @@ import { SkyMap } from './skyMapApi';
 
 // $(document).ready(function() {
 //   $('#submit').click(function() {
-//     const symptoms = $("#userSymptoms").val();
-//     const state = $("#userState").val();
-//     let providerInfo=[];
+//     // const symptoms = $("#userSymptoms").val();
+//     // const state = $("#userState").val();
+//     // let providerInfo=[];
 //
+// //
+//     let constellationInfo = new SkyMap();
+//     let promise = results.getConstInfo();  // call the instance method and pass in user input
 //
-//     let results = new DocSearch();
-//     let promise = results.searchparameters(state,symptoms);  // call the instance method and pass in user input
-//
-//
+// //
 //     promise.then(function(response) {
 //       const body = JSON.parse(response);
-//       for(let i=0; i < 10; i++) {
-//         providerInfo[i] = body.data[i].practices;
-// //forech loop targeting the body.practices
-//         $("#location").append(`<div id=q${i} class="question col-md-4">
-//          <h4 class="category">${body.results[i].category}</h4>
-//
-//       }
-//
-//        $('.doctor').html( `Displaying a list of providers in ${state} ,  ${body.data[0,1].profile}`);
-//
-//
-//     }, function(error) {
-//       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-//     });
+//       console.log(body);
+//forech loop targeting the body.practices
+        // $("#location").append(`<div id=q${i} class="question col-md-4">
+        //  <h4 class="category">${body.results[i].category}</h4>
+
+
+        // $('.constellationInfo').html( `${}`);
+//         // $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+//     })
 //   });
 // });
