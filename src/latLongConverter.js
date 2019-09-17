9bc35648cbe8c4a758da54ca6ebc3777804ba129
christine frank <christine.braun13@gmail.com>
2019-09-17 16:46:44 -0700

    Add converter for lat long to celestial coords

--- src/latLongConverter.js
+++ src/latLongConverter.js
@@ -1,4 +1,4 @@
-export class latLongConverter {
+export class LatLongConverter {
   constructor(lat, long) {
     //latitude
     this.latitude = lat;
@@ -7,12 +7,12 @@ export class latLongConverter {
 
     //assume looking straight up in sky
     this.declination = lat;
+    debugger
+    const julDay = this.convertJulianDay();
+    const theta = this.calcSideReel(julDay, long);
+    const tau = this.calcTau(lat);
 
-    const julDay = convertJulianDay();
-    const theta = calcSideReel(julDay, long);
-    const tau = calcTau(latitude);
-
-    this.rightAscention = calcRa(tau, theta);
+    this.rightAscention = this.calcRa(tau, theta);
   }
 
   convertJulianDay(){
@@ -20,38 +20,60 @@ export class latLongConverter {
     const nowDate = new Date();
 
     const date = nowDate.getDate();
-    const month = nowDate.getMonth();
+    const month = nowDate.getMonth()+1; // base 0 for January
     const year = nowDate.getFullYear();
     const hour = nowDate.getUTCHours();
     const min = nowDate.getUTCMinutes();
     const sec = nowDate.getUTCSeconds();
 
-    const julianDayNum = (1461 × (year + 4800 + (month− 14)/12))/4 +(367 × (month − 2 − 12 × ((month − 14)/12)))/12 − (3 × ((year + 4900 + (month - 14)/12)/100))/4 + day − 32075;
+    let julianDayNum = (367*year) - Math.floor(7*(year + Math.floor((month+9)/12))/4)+Math.floor(275*month/9) + date + 1721013.5;
 
-    if(hour < 12){
-      julianDayNum + 1;
-    }
-    const julianDate = julianDayNum + ((hour-12)/24) + (min/1440) + (sec/86400);
+    const julianDate = julianDayNum + (hour/24) + (min/1440) + (sec/86400);
 
-
-    return julianDate;
+    return [julianDate, hour];
   }
 
   // reference: http://www.geoastro.de/elevaz/basics/index.htm
 
-  calcSideReel(julianDate, longitude){
-    const equinox = 2451545 // JD at vernal equinox
-    const centuriesSinceEq = (julianDate - equinox)/36525;
-    const time = 280.46061837 + 360.98564736629*(julianDate-2451545.0) + 0.000387933*(centuriesSinceEq^2) - (centuriesSinceEq^3)/38710000.0;
+  calcSideReel(julianDateArray, longitude){
+    const julianDate = julianDateArray[0];
+    const utcHour = julianDateArray[1];
+    const midnight = Math.floor(julianDate) +0.5;
+    const daysSinceMid = julianDate - midnight;
+    const hrsSinceMid = daysSinceMid * 24;
 
-    return time + longitude;
+    const equinox = 2451545 // JD at vernal equinox
+    const wholeDaysSinceEq = midnight - equinox;
+    const centuriesSinceEq = (julianDate - equinox)/36525;
+
+    const time = 6.697374558 + (0.06570982441908 * wholeDaysSinceEq)
+     + (1.00273790935 * hrsSinceMid)
+     + (0.000026 * centuriesSinceEq*centuriesSinceEq);
+
+     if(longitude < 0){
+       longitude += 360;
+     } else if (longitude > 360){
+       longitude -= 360;
+     }
+
+    return ((time%24)*15 + longitude) % 360; // convert to degrees then add local location
+  }
+
+  calcEclipLong(julianDateArray){
+    const equinox = 2451545;
+    const julianDate = julianDateArray[0];
+    const julianCenturiesSinceEq = (julianDate-equinox)/36525;
+
+    const eps = 23.0 + (26.0/60.0) + (21.448/3600.0) - (46.8150*julianCenturiesSinceEq + 0.00059*julianCenturiesSinceEq*julianCenturiesSinceEq - 0.001813*julianCenturiesSinceEq*julianCenturiesSinceEq*julianCenturiesSinceEq)/3600;
+
   }
   // hour angle
   // corresponds to the length of sidereal time elapsed since the body St last made a transit of the meridian
   // assume delta declination = 0 (no rotation from celestial equator)
   // assume altitude = 90 (straight up)
-  calcTau(latitude){
-    return Math.acos(1/Math.cos(latitude));
+  calcTau(latitude, longitude){
+    (1-Math.sin(latitude)*Math.sin(90))/(Math.cos(latitude))
+    // return Math.acos(1/Math.cos(latitude));
   }
 
   calcRa(tau, theta){
