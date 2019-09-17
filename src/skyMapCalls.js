5e005d7c0b01ce783fdcb647088f37644553f4d0
christine frank <christine.braun13@gmail.com>
2019-09-17 16:47:41 -0700

    Remove old file ..Api. Correct errors in ..Calls for missing variables

--- src/skyMapCalls.js
+++ src/skyMapCalls.js
@@ -22,7 +22,7 @@ export class SkyMap {
   getConstCoords(constellationName){
     return new Promise((resolve,reject) => {
       let request = new XMLHttpRequest();
-      const url = `${this.coordsUrl}?object=${starName}`;
+      const url = `${this.coordsUrl}?object=${constellationName}`;
 
       request.onload = function(){
         if (this.status === 200) {
@@ -36,7 +36,7 @@ export class SkyMap {
     });
   }
 
-  getGroupCoords(ra, de, rotation, maxStars, maxVMag){
+  getGroupCoords(ra, de, rotation, angle, maxStars, maxVMag){
     return new Promise((resolve,reject) => {
       let request = new XMLHttpRequest();
       const url = `${this.coordsUrl}/getstars.jsp?ra=${ra}&de=${de}&angle=${angle}&&max_stars=${maxStars}&max_vmag=${maxVMag}`;
@@ -77,7 +77,7 @@ export class SkyMap {
   buildStarMap(ra, de, rotation, showBoundaries = 0, showGrid = 0, showConstLines = 0, showConstNames = 0){
     const mapUrlTemplate = `
       http://server7.wikisky.org/map?custom=1&language=EN&type=PART&w=900&h=450
-      &angle=180&ra=${ra}&de=${de}&rotation=${rotation}&mag=10&max_stars=100000&zoom=1000&borders=0&border_color=400000&show_grid=${showGrid}&grid_color=404040&grid_color_zero=808080&grid_lines_width=1.0&grid_ra_step=1.0&grid_de_step=15.0&show_const_lines=${showConstLines}&constellation_lines_color=006000&constellation_lines_width=1.0&show_const_names=${showConstNames}&constellation_names_color=006000&const_name_font_type=PLAIN&const_name_font_name=SanSerif&const_name_font_size=15&show_const_boundaries=0&constellation_boundaries_color=000060&constellation_boundaries_width=1.0&background_color=000000&output=GIF
+      &angle=180&ra=${ra}&de=${de}&rotation=${rotation}&mag=10&max_stars=100000&zoom=1000&borders=0&border_color=400000&show_grid=${showGrid}&grid_color=404040&grid_color_zero=808080&grid_lines_width=1.0&grid_ra_step=1.0&grid_de_step=15.0&show_const_lines=${showConstLines}&constellation_lines_color=006000&constellation_lines_width=1.0&show_const_names=${showConstNames}&constellation_names_color=006000&const_name_font_type=PLAIN&const_name_font_name=SanSerif&const_name_font_size=15&show_const_boundaries=${showBoundaries}&constellation_boundaries_color=000060&constellation_boundaries_width=1.0&background_color=000000&output=GIF
     `;
     return mapUrlTemplate;
   }
